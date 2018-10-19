bf805ecab34ae5d407f2c77a0e2a2ee49ca28f1f
A U Thor <author@example.com>
2018-10-19 14:25:30 +0800

    Updated ToDoListTest

--- test/ToDoListTest.java
+++ test/ToDoListTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 public class ToDoListTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void AddTaskTest() {
+		ToDoList todolist = new ToDoList();
+		Task newTask = new Task(1, "Test", "Testing", 1, false);
+		todolist.addItem(newTask);
+		assertEquals(1, todolist.getItems().size());
+		}
 
 }
